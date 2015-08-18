0de42d2ea1120ab24daae601cf60eccab1c76fcb
Dan Jensen <dj@fitbird.com>
2015-08-18 10:58:04 -0400

    update product reducer

--- src/scripts/reducers/product_reducer.js
+++ src/scripts/reducers/product_reducer.js
@@ -1,8 +1,14 @@
 "use strict";
 import {
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_RESPONSE,
+  PRODUCT_CREATE_ERROR,
   PRODUCT_FETCH_REQUEST,
   PRODUCT_FETCH_RESPONSE,
   PRODUCT_FETCH_ERROR,
+  PRODUCT_DESTROY_REQUEST,
+  PRODUCT_DESTROY_RESPONSE,
+  PRODUCT_DESTROY_ERROR,
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_RESPONSE,
   PRODUCT_UPDATE_ERROR
@@ -16,12 +22,16 @@ const initialState = {
 
 export default function productState(state = initialState, action) {
   switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+    case PRODUCT_DESTROY_REQUEST:
     case PRODUCT_FETCH_REQUEST:
     case PRODUCT_UPDATE_REQUEST:
       return Object.assign({}, state, {
         isFetching: true
       });
 
+    case PRODUCT_CREATE_RESPONSE:
+    case PRODUCT_DESTROY_RESPONSE:
     case PRODUCT_FETCH_RESPONSE:
     case PRODUCT_UPDATE_RESPONSE:
       return Object.assign({}, state, {
@@ -30,7 +40,9 @@ export default function productState(state = initialState, action) {
         products: action.data
       });
 
+    case PRODUCT_CREATE_ERROR:
     case PRODUCT_FETCH_ERROR:
+    case PRODUCT_DESTROY_ERROR:
     case PRODUCT_UPDATE_ERROR:
       return Object.assign({}, state, {
         errors: action.errors,

b662c4203f95fb20587911cdc77c5e25672eb10f
Dan Jensen <dj@fitbird.com>
2015-08-18 19:14:35 -0400

    rename files

--- spec/fetchAllProductsSpec.js
+++ spec/fetchAllProductsSpec.js
@@ -3,12 +3,12 @@
 describe("fetchAllProducts", () => {
   describe("on success", () => {
     it("should return status 200", () => {
-      let fetchAllProducts = require("./fetch_all_products");
+      let fetchAllProducts = require("./fetchAllProducts");
       let response = fetchAllProducts(200);
       expect(response).toEqual(200);
     });
     it("should return all products", () => {
-      let fetchAllProducts = require("./fetch_all_products");
+      let fetchAllProducts = require("./fetchAllProducts");
       let response = fetchAllProducts();
       expect(response).toEqual({});
     });

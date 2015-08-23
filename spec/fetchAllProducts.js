a4d63932898fc6d17a4c53da8f5f0b6f19c49a98
Tom Chen <developer@tomchentw.com>
2015-08-23 10:53:53 +0800

    feat(fetchAllProducts): fix async test using jasmine-ajax

--- spec/fetchAllProducts.js
+++ spec/fetchAllProducts.js
@@ -1,13 +1,12 @@
 "use strict";
 import $ from "jquery";
 
-export default function fetchAllProducts(status, result = {}) {
-  return (status === 200) ? status : result
-  // return (
-  //   $.ajax({
-  //     url: "http://localhost:3000/api/products",
-  //     dataType: "json",
-  //     type: "GET"
-  //   })
-  // );
+export default function fetchAllProducts() {
+  return Promise.resolve(
+    $.ajax({
+      url: "http://localhost:3000/api/products",
+      dataType: "json",
+      type: "GET"
+    })
+  );
 }
--- spec/fetchAllProductsSpec.js
+++ spec/fetchAllProductsSpec.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import fetchAllProducts from "./fetchAllProducts";
+
 describe("fetchAllProducts", () => {
   beforeEach(function() {
     jasmine.Ajax.install();
@@ -8,17 +10,39 @@ describe("fetchAllProducts", () => {
   afterEach(function() {
     jasmine.Ajax.uninstall();
   });
-  
+
   describe("on success", () => {
-    it("should return status 200", () => {
-      let fetchAllProducts = require("./fetchAllProducts");
-      let response = fetchAllProducts(200);
-      expect(response).toEqual(200);
+    it("should return status 200", (done) => { // done means async test
+      // mock response for certain endpoint
+      jasmine.Ajax.stubRequest("http://localhost:3000/api/products").andReturn({
+        "responseText": JSON.stringify({data: []})
+      });
+
+      // Because fetchAllProducts returns a promise, we have to wait until
+      // it resolves.
+      fetchAllProducts().then((body) => {
+        expect(body).toEqual({data: []});
+        
+        done();
+      });
     });
+
     it("should return all products", () => {
-      let fetchAllProducts = require("./fetchAllProducts");
-      let response = fetchAllProducts();
-      expect(response).toEqual({});
+      // mock response for certain endpoint
+      jasmine.Ajax.stubRequest("http://localhost:3000/api/products").andReturn({
+        "responseText": JSON.stringify({data: [
+          { name: "aaa", price: 100 },
+          { name: "bbb", price: 200 },
+        ]})
+      });
+
+      // Because fetchAllProducts returns a promise, we have to wait until
+      // it resolves.
+      fetchAllProducts().then((body) => {
+        expect(body.data.length).toEqual(2);
+        
+        done();
+      });
     });
   });
 });
